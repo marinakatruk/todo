66ab222b154fe38f158bc4cbdb5987abe2f958d2
Marina Katruk <m.katruk@outlook.com>
2020-12-03 15:37:42 +0300

    Updates

---
+++ precache-manifest.66a6c2050751b6090ec98602ebe8c9a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d95a1df2e2083543655596321323c46",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "37952ffc3d2ff8f095cf",
+    "url": "/todo/static/css/main.a8e959c1.chunk.css"
+  },
+  {
+    "revision": "631a84564d169ea84bea",
+    "url": "/todo/static/js/2.762090d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.762090d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37952ffc3d2ff8f095cf",
+    "url": "/todo/static/js/main.d77d8fce.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
